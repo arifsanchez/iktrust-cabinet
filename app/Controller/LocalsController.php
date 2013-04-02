e9e9a6d8980caa9d7334e21e28e6d0e2b20dd978
intan nabila <intannabilasalim@gmail.com>
2013-04-02 16:30:52 +0800

    test get id

--- app/Controller/LocalsController.php
+++ app/Controller/LocalsController.php
@@ -1,10 +1,30 @@
 <?php
 App::uses('AppController', 'Controller');
+App::uses('HttpSocket', 'Network/Http');
 
 class LocalsController extends AppController {
 
 
+	public function test(){
+	
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_URL, "http://iktrust.co.uk/webservice/api.php");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, true);
 
+		// hantar parameter symbols dgn value EURUSD
+		$data = array(
+			'action' => 'getSpecifications',
+			'symbols' => 'EURUSD'
+		);
+
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		$output = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		debug($output);die();
+	
+	}
 
 
 }
