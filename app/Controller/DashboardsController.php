9dcee64eafe980ad434925317540bed74881fc65
paihz <mohdfaihz@gmail.com>
2013-04-08 13:01:33 +0800

    testt add

---
+++ app/Controller/DashboardsController.php
@@ -0,0 +1,73 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class DashboardsController extends AppController {
+
+	public function add_group() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function add_users() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function all_groups() {
+			$this->layout = 'kabinet';
+			
+		}
+	
+	public function all_pages() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function all_settings() {
+			$this->layout = 'kabinet';
+			
+		}
+	
+	public function all_users() {
+			$this->layout = 'kabinet';
+			
+		}
+	
+	
+	public function change_password() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	
+	public function dashboard() {
+			$this->layout = 'kabinet';
+			
+		}
+	
+	public function delete_cache() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function edit_profile() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function online_users() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function permissions() {
+			$this->layout = 'kabinet';
+			
+		}
+		
+	public function profile() {
+			$this->layout = 'kabinet';
+			
+		}
+	
+}
